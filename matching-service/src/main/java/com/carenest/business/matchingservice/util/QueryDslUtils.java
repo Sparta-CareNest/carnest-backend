cc86e62cc27801676bf4ca072c149099ae54e1fe
Jong-Min <codejomo99@gmail.com>
2025-04-06 17:15:41 +0900

    chore: Utils 추가 #5

---
+++ matching-service/src/main/java/com/carenest/business/matchingservice/util/QueryDslUtils.java
@@ -0,0 +1,24 @@
+package com.carenest.business.matchingservice.util;
+
+import org.springframework.data.domain.Sort;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+
+public class QueryDslUtils {
+
+	// Sort 기반 동적 정렬을 적용할 때 유용
+	public static OrderSpecifier<?>[] getOrderSpecifiers(Sort sort, Class<?> entityClass) {
+		String str = entityClass.getSimpleName();
+		PathBuilder<?> entityPath = new PathBuilder<>(entityClass, Character.toLowerCase(str.charAt(0)) + str.substring(1) );
+		return sort.stream()
+			.map(order -> new OrderSpecifier<>(
+					order.isAscending() ? Order.ASC : Order.DESC,
+					entityPath.getString(order.getProperty())
+				)
+			)
+			.toList()
+			.toArray(new OrderSpecifier[0]);
+	}
+}
