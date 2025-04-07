2510f6f89ea6700026b2714deb713dd66e2906e4
jeonges <dmstjs915@gmail.com>
2025-04-07 07:49:13 -0700

    feat: 알림 엔티티 생성

---
+++ notification-service/src/main/java/com/carenest/business/notificationservice/domain/model/Notification.java
@@ -0,0 +1,45 @@
+package com.carenest.business.notificationservice.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "notification_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "receiver_id", nullable = false)
+    private UUID receiverId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "is_read", nullable = false)
+    private boolean isRead;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "sent_at")
+    private LocalDateTime sentAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
