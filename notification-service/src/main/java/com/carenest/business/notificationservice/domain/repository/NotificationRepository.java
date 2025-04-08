7eeaa0a05749ebe6015edccb40b00cebcc82971d
jeonges <dmstjs915@gmail.com>
2025-04-08 10:05:54 -0700

    feat: 알림 Repository 생성

---
+++ notification-service/src/main/java/com/carenest/business/notificationservice/domain/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.carenest.business.notificationservice.domain.repository;
+
+import com.carenest.business.notificationservice.domain.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+}
