518406cc000c9a3f697e4e3ecb56fb576145e082
Jong-Min <codejomo99@gmail.com>
2025-04-09 16:20:05 +0900

    feat: 간병인 검색 기능 QueryDsl 구현 #35

--- caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/repository/CaregiverRepository.java
+++ caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/repository/CaregiverRepository.java
@@ -5,7 +5,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carenest.business.caregiverservice.domain.model.Caregiver;
+import com.carenest.business.caregiverservice.infrastructure.repository.querydsl.CaregiverCustomRepository;
 
-public interface CaregiverRepository extends JpaRepository<Caregiver, UUID> {
+public interface CaregiverRepository extends JpaRepository<Caregiver, UUID>, CaregiverCustomRepository {
 
 }
--- caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/repository/querydsl/CaregiverRepositoryImpl.java
+++ caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/repository/querydsl/CaregiverRepositoryImpl.java
@@ -1,5 +1,22 @@
 package com.carenest.business.caregiverservice.infrastructure.repository.querydsl;
 
+import static com.carenest.business.caregiverservice.domain.model.QCaregiver.*;
+import static com.carenest.business.caregiverservice.domain.model.category.QCaregiverCategoryLocation.*;
+import static com.carenest.business.caregiverservice.domain.model.category.QCaregiverCategoryService.*;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+
+import com.carenest.business.caregiverservice.domain.model.Caregiver;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -7,4 +24,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CaregiverRepositoryImpl implements CaregiverCustomRepository {
 	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public Page<Caregiver> searchByConditions(String location, String service, Pageable pageable) {
+
+		JPAQuery<Caregiver> careQuery = jpaQueryFactory
+			.selectFrom(caregiver)
+			.leftJoin(caregiver.caregiverCategoryLocations, caregiverCategoryLocation).fetchJoin()
+			.leftJoin(caregiver.caregiverCategoryServices, caregiverCategoryService).fetchJoin()
+			.where(
+				location != null ? caregiverCategoryLocation.categoryLocation.name.eq(location) : null,
+				service != null ? caregiverCategoryService.categoryService.name.eq(service) : null
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize());
+
+		// 정렬
+		for(Sort.Order o : pageable.getSort()){
+			PathBuilder pathBuilder = new PathBuilder(caregiver.getType(), caregiver.getMetadata());
+			careQuery.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+		}
+
+		List<Caregiver> caregivers = careQuery.fetch();
+		JPAQuery<Long> caregiverCountQuery = jpaQueryFactory
+			.select(caregiver.count())
+			.from(caregiver)
+			.leftJoin(caregiver.caregiverCategoryServices, caregiverCategoryService)
+			.leftJoin(caregiver.caregiverCategoryLocations, caregiverCategoryLocation)
+			.where(
+				location != null ? caregiverCategoryLocation.categoryLocation.name.eq(location) : null,
+				service != null ? caregiverCategoryService.categoryService.name.eq(service) : null
+			);
+
+		return new PageImpl<>(caregivers,pageable,caregiverCountQuery.fetchOne());
+	}
 }
