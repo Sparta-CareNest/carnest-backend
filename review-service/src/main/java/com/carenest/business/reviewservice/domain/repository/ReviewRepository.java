436c5b0698c3447b69486422e7bd0039c464c672
dydwo6018 <dydwo6018@gmail.com>
2025-04-09 10:54:05 +0900

    feat: ReviewRepository Read 기능 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/domain/repository/ReviewRepository.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/domain/repository/ReviewRepository.java
@@ -2,6 +2,12 @@ package com.carenest.business.reviewservice.domain.repository;
 
 import com.carenest.business.reviewservice.domain.model.Review;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ReviewRepository {
     Review save(Review review);
+    Optional<Review> findById(UUID orderId);
+    List<Review> findAll();
 }
