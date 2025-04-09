f058423d667db994d55f369da48f3bd261d54b06
dydwo6018 <dydwo6018@gmail.com>
2025-04-09 11:22:34 +0900

    어노테이션 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/application/dto/request/ReviewCreateRequestDto.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/application/dto/request/ReviewCreateRequestDto.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
 public class ReviewCreateRequestDto {
     @NotNull
     private UUID reservationId;
@@ -18,7 +20,7 @@ public class ReviewCreateRequestDto {
 
     @Min(1)
     @Max(5)
-    private int rating;
+    private double rating;
 
     @NotBlank
     private String content;
