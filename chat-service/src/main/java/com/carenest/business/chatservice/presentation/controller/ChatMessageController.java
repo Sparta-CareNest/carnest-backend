0cea734c9ef6e80daf43b08d138cbee2d184383a
jeonges <dmstjs915@gmail.com>
2025-04-10 03:12:01 -0700

    feat: 메세지 송신 컨트롤러 추가

---
+++ chat-service/src/main/java/com/carenest/business/chatservice/presentation/controller/ChatMessageController.java
@@ -0,0 +1,33 @@
+package com.carenest.business.chatservice.presentation.controller;
+
+import com.carenest.business.chatservice.application.dto.request.ChatMessageRequestDto;
+import com.carenest.business.chatservice.application.dto.response.ChatMessageResponseDto;
+import com.carenest.business.chatservice.application.service.ChatMessageService;
+import com.carenest.business.chatservice.application.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatMessageService chatMessageService;
+
+    @MessageMapping("/chat/message") // 클라이언트는 /pub/chat/message 로 메시지 보냄
+    public void sendMessage(@Payload ChatMessageRequestDto messageDto) {
+        chatMessageService.sendMessage(messageDto);
+
+        messagingTemplate.convertAndSend(
+                "/sub/chat/rooms/" + messageDto.getChatRoomId(),
+                ChatMessageResponseDto.builder()
+                        .id(messageDto.getChatRoomId())
+                        .senderId(messageDto.getSenderId())
+                        .message(messageDto.getMessage())
+                        .build()
+        );
+    }
+}
