b286c36ed856ccac6e4076f3ea6210b79c81d756
Jong-Min <codejomo99@gmail.com>
2025-04-11 00:29:05 +0900

    refactor: 승인하는 파일들 ElementCollection로 별도 컬렉션 테이블로 관리 #50

--- caregiver-service/src/main/java/com/carenest/business/caregiverservice/domain/model/Caregiver.java
+++ caregiver-service/src/main/java/com/carenest/business/caregiverservice/domain/model/Caregiver.java
@@ -1,6 +1,8 @@
 package com.carenest.business.caregiverservice.domain.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -12,14 +14,18 @@ import com.carenest.business.caregiverservice.domain.model.category.CaregiverCat
 import com.carenest.business.common.entity.BaseEntity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -69,8 +75,12 @@ public class Caregiver extends BaseEntity {
 	@Column
 	@Builder.Default
 	private Boolean approvalStatus = false;
-	@Column
-	private String submittedDocuments;
+
+	@ElementCollection(fetch = FetchType.LAZY)
+	@CollectionTable(name = "p_caregiver_document_urls", joinColumns = @JoinColumn(name = "caregiver_id"))
+	@Column(name = "document_url")
+	@Builder.Default
+	private List<String> submittedDocuments = new ArrayList<>();
 
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
