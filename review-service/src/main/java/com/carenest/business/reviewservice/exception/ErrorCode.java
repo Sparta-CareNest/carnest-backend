2f6e405bf2d06f13954253587fb2930594c8d100
dydwo6018 <dydwo6018@gmail.com>
2025-04-13 17:34:42 +0900

    feat: Caregiver 유효성 관련 ErrorCode 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
 
     REVIEW_NOT_FOUND(404, "리뷰를 찾을 수 없습니다."),
     INVALID_REVIEW_CONTENT(400, "리뷰 내용이 유효하지 않습니다."),
-    UNAUTHORIZED_REVIEW_DELETE(403, "리뷰 삭제 권한이 없습니다.");
+    UNAUTHORIZED_REVIEW_DELETE(403, "리뷰 삭제 권한이 없습니다."),
+    INVALID_CAREGIVER(404, "유효하지 않은 간병인입니다.");
+
 
     private final int status;
     private final String message;
