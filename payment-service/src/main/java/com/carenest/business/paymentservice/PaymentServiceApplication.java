b18a58396a2ccc9ec4533ba4d6654be3341ed37e
mango606 <yuj08184@kangwon.ac.kr>
2025-04-14 01:29:16 +0900

    feat: Feign Client 설정 및 CircuitBreaker 설정 추가

--- payment-service/src/main/java/com/carenest/business/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/carenest/business/paymentservice/PaymentServiceApplication.java
@@ -2,12 +2,15 @@ package com.carenest.business.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
-
 }
