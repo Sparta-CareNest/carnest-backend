706e2550c3caaafc0703edc5a9d0196a29bf7f39
jeonges <dmstjs915@gmail.com>
2025-04-13 11:02:30 -0700

    feat: 서비스에 userclient 적용

--- notification-service/src/main/java/com/carenest/business/notificationservice/application/service/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/carenest/business/notificationservice/application/service/NotificationServiceImpl.java
@@ -2,9 +2,11 @@ package com.carenest.business.notificationservice.application.service;
 
 import com.carenest.business.notificationservice.application.dto.request.NotificationCreateRequestDto;
 import com.carenest.business.notificationservice.application.dto.response.NotificationResponseDto;
+import com.carenest.business.notificationservice.application.dto.response.UserInfoResponseDto;
 import com.carenest.business.notificationservice.domain.model.Notification;
 import com.carenest.business.notificationservice.domain.model.NotificationType;
 import com.carenest.business.notificationservice.domain.repository.NotificationRepository;
+import com.carenest.business.notificationservice.infrastructure.client.UserClient;
 import com.carenest.business.notificationservice.infrastructure.config.NotificationWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,16 +22,30 @@ public class NotificationServiceImpl implements NotificationService{
 
     private final NotificationRepository notificationRepository;
     private final NotificationWebSocketHandler webSocketHandler;
+    private final UserClient userClient;
 
     @Override
     public NotificationResponseDto createNotificationWithType(
             NotificationCreateRequestDto requestDto,
             NotificationType notificationType
     ) {
+        // 유저 존재 여부 확인
+        boolean isValidUser = userClient.validateUser(requestDto.getReceiverId());
+        if (!isValidUser) {
+            throw new IllegalArgumentException("존재하지 않는 유저에게 알림을 보낼 수 없습니다.");
+        }
+
+        // 유저 닉네임 가져오기
+        UserInfoResponseDto userInfo = userClient.getUserInfo(requestDto.getReceiverId());
+        String nickname = userInfo.getNickname();
+
+        // 알림 내용에 닉네임 포하
+        String content = "[" + nickname + "] " + notificationType.getMessage();
+
         Notification notification = Notification.create(
                 requestDto.getReceiverId(),
                 notificationType,
-                notificationType.getMessage()
+                content
         );
 
         Notification saved = notificationRepository.save(notification);
