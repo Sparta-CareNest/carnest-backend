a207a02e8463d4fe390c7a75d048c9c13103d1ac
dydwo6018 <dydwo6018@gmail.com>
2025-04-13 17:35:12 +0900

    feat: FeignClient 관련 어노테이션 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.carenest.business.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients(basePackages = "com.carenest.business.reviewservice.infrastructure.client")
 @SpringBootApplication
 @EnableJpaAuditing
 public class ReviewServiceApplication {
