a55201f5b6c64a29c630af56d1b255640822cfae
jeonges <dmstjs915@gmail.com>
2025-04-14 08:32:18 -0700

    feat: @EnableFeignClients 수정

--- notification-service/src/main/java/com/carenest/business/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/carenest/business/notificationservice/NotificationServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.carenest.business.notificationservice.infrastructure.client")
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
