577006d78961c132ec25077af0c3319062f18b94
jeonges <dmstjs915@gmail.com>
2025-04-14 08:47:28 -0700

    refactor: import 수정

--- notification-service/src/main/java/com/carenest/business/notificationservice/application/service/NotificationService.java
+++ notification-service/src/main/java/com/carenest/business/notificationservice/application/service/NotificationService.java
@@ -1,7 +1,6 @@
 package com.carenest.business.notificationservice.application.service;
 
 import com.carenest.business.notificationservice.application.dto.request.NotificationCreateRequestDto;
-import com.carenest.business.notificationservice.application.dto.request.ReservationNotificationRequestDto;
 import com.carenest.business.notificationservice.application.dto.response.NotificationResponseDto;
 import com.carenest.business.notificationservice.domain.model.NotificationType;
 
