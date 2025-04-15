650c20226cc57ad58bf833b1cc65b5e26b73d5d3
dydwo6018 <dydwo6018@gmail.com>
2025-04-15 12:20:46 +0900

    Error 코드 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     REVIEW_NOT_FOUND(404, "리뷰를 찾을 수 없습니다."),
     INVALID_REVIEW_CONTENT(400, "리뷰 내용이 유효하지 않습니다."),
     UNAUTHORIZED_REVIEW_DELETE(403, "리뷰 삭제 권한이 없습니다."),
-    INVALID_CAREGIVER(404, "유효하지 않은 간병인입니다.");
+    INVALID_USER(404, "존재하지 않는 사용자입니다."),
+    INVALID_CAREGIVER(404, "존재하지 않은 간병인입니다.");
 
 
     private final int status;
