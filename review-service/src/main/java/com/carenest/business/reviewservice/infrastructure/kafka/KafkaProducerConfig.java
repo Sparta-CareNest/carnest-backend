6b35fd998c5d4d64cb9750321f7bce96c72b0e2c
Jong-Min <codejomo99@gmail.com>
2025-04-15 14:17:26 +0900

    feat: kafka Producer config #84

---
+++ review-service/src/main/java/com/carenest/business/reviewservice/infrastructure/kafka/KafkaProducerConfig.java
@@ -0,0 +1,32 @@
+package com.carenest.business.reviewservice.infrastructure.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+
+	// 프로듀서 설정 정의
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		Map<String, Object> config = new HashMap<>();
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka 브로커 주소
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return new DefaultKafkaProducerFactory<>(config);
+	}
+
+	// KafkaTemplate은 메시지 전송을 쉽게 해줌
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+}
