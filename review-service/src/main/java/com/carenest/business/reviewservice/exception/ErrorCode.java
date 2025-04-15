3d448429a1ffd9cf27a63c645c20bf6f075c3fe8
dydwo6018 <dydwo6018@gmail.com>
2025-04-15 12:30:56 +0900

    refactor: ErrorCode 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     INVALID_REVIEW_CONTENT(400, "리뷰 내용이 유효하지 않습니다."),
     UNAUTHORIZED_REVIEW_DELETE(403, "리뷰 삭제 권한이 없습니다."),
     INVALID_USER(404, "존재하지 않는 사용자입니다."),
-    INVALID_CAREGIVER(404, "존재하지 않은 간병인입니다.");
+    INVALID_CAREGIVER(404, "존재하지 않은 간병인입니다."),
+    UNAUTHORIZED_REVIEW_ACCESS(403, "리뷰 수정 또는 삭제 권한이 없습니다.");
 
 
     private final int status;
