cce0dcf556683072b5e5355b456d6fbd620d4a68
Jong-Min <codejomo99@gmail.com>
2025-04-17 11:50:23 +0900

    refactor: description 추가

--- review-service/src/main/java/com/carenest/business/reviewservice/infrastructure/kafka/KafkaTopic.java
+++ review-service/src/main/java/com/carenest/business/reviewservice/infrastructure/kafka/KafkaTopic.java
@@ -1,15 +1,21 @@
 package com.carenest.business.reviewservice.infrastructure.kafka;
 
 public enum KafkaTopic {
-	REVIEW_RATING_UPDATE("review-rating-update");
+	REVIEW_RATING_UPDATE("review-rating-update", "리뷰 평점 업데이트 토픽");
 
 	private final String topicName;
+	private final String description;
 
-	KafkaTopic(String topicName) {
+	KafkaTopic(String topicName, String description) {
 		this.topicName = topicName;
+		this.description = description;
 	}
 
 	public String getTopicName() {
 		return topicName;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 }
