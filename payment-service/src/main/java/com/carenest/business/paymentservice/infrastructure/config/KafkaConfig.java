0fc29172bf9451ef4b90a210b26e5438efa37e42
mango606 <yuj08184@kangwon.ac.kr>
2025-04-18 15:08:30 +0900

    refactor: Kafka 처리 오류 시 재시도 설정 추가

--- payment-service/src/main/java/com/carenest/business/paymentservice/infrastructure/config/KafkaConfig.java
+++ payment-service/src/main/java/com/carenest/business/paymentservice/infrastructure/config/KafkaConfig.java
@@ -61,6 +61,8 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
+        configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
