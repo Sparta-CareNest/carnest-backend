f62f2c52ec05d21c0796ca9b13b34995e717e99a
jeonges <dmstjs915@gmail.com>
2025-04-20 12:42:40 -0700

    feat: SettlementKafkaProducer 생성

---
+++ admin-service/src/main/java/com/carenest/business/adminservice/infrastructure/kafka/SettlementKafkaProducer.java
@@ -0,0 +1,34 @@
+package com.carenest.business.adminservice.infrastructure.kafka;
+
+import com.carenest.business.adminservice.infrastructure.dto.SettlementCreatedEventDto;
+import com.carenest.business.common.event.admin.SettlementCreatedEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SettlementKafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public SettlementKafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendSettlementNotification(String topic, SettlementCreatedEvent event) {
+        String message = String.format(
+                "settlementId=%s, careWorkerId=%s, amount=%s, periodStart=%s, periodEnd=%s, settledAt=%s",
+                event.getSettlementId(),
+                event.getCareWorkerId(),
+                event.getAmount(),
+                event.getPeriodStart(),
+                event.getPeriodEnd(),
+                event.getSettledAt()
+        );
+
+        kafkaTemplate.send(topic, message);
+        log.info("[Kafka] 메시지 전송 성공 - topic: {}, message: {}", topic, message);
+    }
+}
