c5ce7091a6f5f0c3977050fc90ed732efe565d4f
dydwo6018 <dydwo6018@gmail.com>
2025-04-22 10:49:13 +0900

    feat: 자연어 입력 받는 DTO

---
+++ ai-service/src/main/java/com/carenest/business/aiservice/application/dto/CaregiverRecommendRequestDto.java
@@ -0,0 +1,10 @@
+package com.carenest.business.aiservice.application.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CaregiverRecommendRequestDto {
+    private String query;
+}
