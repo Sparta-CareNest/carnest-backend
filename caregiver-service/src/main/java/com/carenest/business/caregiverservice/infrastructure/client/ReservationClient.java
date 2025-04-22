73454c6231f90fdfcf38b46217f29acab2c8a997
Jong-Min <codejomo99@gmail.com>
2025-04-22 12:43:14 +0900

    feat: ReservationClient 승인/거절 api 추가 #127

--- caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/client/ReservationClient.java
+++ caregiver-service/src/main/java/com/carenest/business/caregiverservice/infrastructure/client/ReservationClient.java
@@ -4,18 +4,30 @@ import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.carenest.business.caregiverservice.config.FeignConfig;
+import com.carenest.business.caregiverservice.infrastructure.client.dto.reservation.ReservationAcceptRequest;
+import com.carenest.business.caregiverservice.infrastructure.client.dto.reservation.ReservationRejectRequest;
 import com.carenest.business.caregiverservice.infrastructure.client.dto.reservation.ReservationResponse;
 import com.carenest.business.common.response.ResponseDto;
 
-@FeignClient(
-	name = "reservation-service",
-	configuration = FeignConfig.class
-)
+import jakarta.validation.Valid;
+
+@FeignClient(name = "reservation-service", configuration = FeignConfig.class)
 public interface ReservationClient {
 
 	@GetMapping("/api/v1/internal/reservations/{reservationId}")
 	ResponseDto<ReservationResponse> getReservationDetails(@PathVariable UUID reservationId);
+
+	@PatchMapping("/reservations/{reservationId}/accept")
+	ResponseDto<ReservationResponse> acceptReservation(@PathVariable UUID reservationId,
+		@RequestBody @Valid ReservationAcceptRequest request);
+
+	@PatchMapping("/reservations/{reservationId}/reject")
+	ResponseDto<ReservationResponse> rejectReservation(@PathVariable UUID reservationId,
+		@RequestBody @Valid ReservationRejectRequest request);
 }
+
